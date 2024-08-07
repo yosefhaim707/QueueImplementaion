5e9f598de563d65cb9cf873583a6bff916d67d52
yosefhaim707 <yosefhaimmalamud@gmail.com>
2024-08-07 12:12:44 +0300

    Add project files.

---
+++ QueueImplementaion/SampleDictionary.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueImplementaion
+{
+    internal class SampleDictionary
+    {
+        public Dictionary<string, string> Dictionary { get; set; }
+        public SampleDictionary()
+        {
+            Dictionary = new Dictionary<string, string>();
+        }
+
+        // A few common methods of Dictionary
+
+        // Add
+        public void Add(string key, string value)
+        {
+            Dictionary.Add(key, value);
+        }
+
+        // Remove
+        public void Remove(string key)
+        {
+            if (Dictionary.ContainsKey(key))
+            {
+                Dictionary.Remove(key);
+            }
+            else
+            {
+                Console.WriteLine("Key not found");
+            }
+        }
+
+        // ContainsKey
+        public void ContainsKey(string key)
+        {
+            if (Dictionary.ContainsKey(key))
+            {
+                Console.WriteLine("Dictionary contains key " + key);
+            }
+            else
+            {
+                Console.WriteLine("Dictionary does not contain key " + key);
+            }
+        }
+
+        // ContainsValue
+        public void ContainsValue(string value)
+        {
+            if (Dictionary.ContainsValue(value))
+            {
+                Console.WriteLine("Dictionary contains value " + value);
+            }
+            else
+            {
+                Console.WriteLine("Dictionary does not contain value " + value);
+            }
+        }
+
+        // Set Item
+        public void SetItem(string key, string value)
+        {
+            if (Dictionary.ContainsKey(key))
+            {
+                Dictionary[key] = value;
+            }
+            else
+            {
+                Console.WriteLine("Key not found");
+            }
+        }
+
+        // Clear
+        public void Clear()
+        {
+            Dictionary.Clear();
+        }
+    }
+}
---
+++ QueueImplementaion/SampleQueue.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueImplementaion
+{
+   internal class SampleQueue
+    {
+        public Queue<int> Queue { get; set; }
+        public SampleQueue()
+        {
+            Queue = new Queue<int>();
+        }
+
+        // A few common methods of Queue
+        
+        // Enqueue
+        public void Enqueue(int value)
+        {
+            Queue.Enqueue(value);
+        }
+
+        // Dequeue
+        public void Dequeue()
+        {
+            if (Queue.Count > 0)
+            {
+                Queue.Dequeue();
+            }
+            else
+            {
+                Console.WriteLine("Queue is empty");
+            }
+        }
+
+        // Peek
+        public void Peek()
+        {
+            if (Queue.Count > 0)
+            {
+                Console.WriteLine(Queue.Peek());
+            }
+            else
+            {
+                Console.WriteLine("Queue is empty");
+            }
+        }
+
+        // Contains
+        public void Contains(int value)
+        {
+            if (Queue.Contains(value))
+            {
+                Console.WriteLine("Queue contains " + value);
+            }
+            else
+            {
+                Console.WriteLine("Queue does not contain " + value);
+            }
+        }
+
+        // Clear
+        public void Clear()
+        {
+            Queue.Clear();
+        }
+    }
+}
